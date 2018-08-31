82fc7cbaa3f7d45d8fb2d42a4c62fc3055f4c02f
Peter Martinson <peter.j.martinson@gmail.com>
2018-08-31 06:01:42 -0400

    Experiment with prop validation

--- 02/prop/prop.js
+++ 02/prop/prop.js
@@ -1,3 +1,13 @@
+Vue.component('horace', {
+  props: {
+    text: {
+      type: Number, // This should fail validation, and show an error in the browser console
+      required: true
+    }
+  },
+  template: `<div>{{ text }} </div>`
+});
+
 Vue.component('gordon', {
   props: ['text'],
   template: `<div>{{ text }} </div>`
---
+++ notes.md
@@ -0,0 +1,7 @@
+### Props
+
+    <tag v-bind:text="message"></tag>
+
+`message` is defined in the instance
+`text` is a prop defined in the component
+We are binding the component's prop to the instance's data.
