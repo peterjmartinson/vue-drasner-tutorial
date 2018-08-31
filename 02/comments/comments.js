6cae4270b369c550eb38cb722ad2738dcc4f4cf9
Peter Martinson <peter.j.martinson@gmail.com>
2018-08-31 06:42:38 -0400

    Try to use template literals, for real, and fail
    You need to use Bable for this to work.

--- 02/comments/comments.js
+++ 02/comments/comments.js
@@ -3,21 +3,38 @@ Vue.component('individual-comment', {
   props: ['commentpost']
 });
 
+// This doesn't work, probably because you're not using Babel
 new Vue({
   el: '#app',
   data: {
     newComment: '',
     comments: [
-      'Looks great yo!',
-      'I love the sea',
-      'Where you at?'
+      { 
+        text: 'Looks great Julianne!',
+        author: 'Robin Rendle',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-coffee.jpg'
+      },
+      { 
+        text: 'I love the Sea',
+        author: 'Miriam Suzanne',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-miriam.jpg'
+      },
+      { 
+        text: 'Where are you?',
+        author: 'Geoff Graham',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-geoff.jpg'
+      }
     ]
   },
   methods: {
-    addComment: function() {
-      this.comments.push(this.newComment);
+    addComment: function () {
+      const newCommentObj = {
+        text: this.newComment,
+        author: 'Magoo',
+        authorImg: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/v-skull.jpg'
+      };
+      this.comments.push(newCommentObj);
       this.newComment = '';
     }
-
   }
 });
